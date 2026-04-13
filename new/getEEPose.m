91b706bcdeb5e6c24f5d302d3265426553a7ab47
Shivam <shivamkataria1000@gmail.com>
2026-04-12 18:04:41 -0700

    forward kinematics EE position fix

--- new/getEEPose.m
+++ new/getEEPose.m
@@ -1,12 +1,27 @@
 function [pos, rot, rpy] = getEEPose(theta)
     [~,~,~,~,~,T06] = forwardKinematics(theta);
-    pos = T06(1:3, 4);
     rot = T06(1:3, 1:3);
     
-    % Extract roll, pitch, yaw from rotation matrix
-    pitch = atan2(-rot(3,1), sqrt(rot(1,1)^2 + rot(2,1)^2));
-    yaw   = atan2(rot(2,1)/cos(pitch), rot(1,1)/cos(pitch));
-    roll  = atan2(rot(3,2)/cos(pitch), rot(3,3)/cos(pitch));
+    % Actual EE position with CAD offset
+    offset_dh = [-0.341; -0.736; 3.015];
+    pos = T06(1:3, 4) + rot * offset_dh;
+    
+    % Robust ZYX euler angles (yaw, pitch, roll)
+    if abs(rot(3,1)) < 1 - 1e-10
+        pitch = asin(-rot(3,1));
+        roll  = atan2(rot(3,2)/cos(pitch), rot(3,3)/cos(pitch));
+        yaw   = atan2(rot(2,1)/cos(pitch), rot(1,1)/cos(pitch));
+    else
+        % Gimbal lock
+        yaw = 0;
+        if rot(3,1) < 0
+            pitch = pi/2;
+            roll  = atan2(rot(1,2), rot(1,3));
+        else
+            pitch = -pi/2;
+            roll  = atan2(-rot(1,2), -rot(1,3));
+        end
+    end
     
     rpy = rad2deg([roll; pitch; yaw]);
 end
