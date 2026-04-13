63158959cbee792bcb03701fa14863508d9427f0
Shivam <shivamkataria1000@gmail.com>
2026-04-12 19:01:23 -0700

    idk anymore

--- new/getEEPose.m
+++ new/getEEPose.m
@@ -1,5 +1,4 @@
 function [pos, rot, rpy] = getEEPose(theta)
-    % Use same joint data as updateRobot
     origins = [0.4500  0.0000  1.2446;
                0.7206  0.3986  1.2455;
                7.7567  0.9602  1.2461;
@@ -14,57 +13,47 @@ function [pos, rot, rpy] = getEEPose(theta)
             1  0  0;
             0  1  0];
 
-    % Start with EE center at zero position
     ee_pos = [15.522; 3.015; 1.244];
-    
-    % Track cumulative rotation
     R_total = eye(3);
     cur_origins = origins;
+    cur_axes = axes;
 
-    % Apply each joint sequentially - same as updateRobot
     for j = 1:6
         origin = cur_origins(j,:)';
-        ax = axes(j,:)';
+        ax = cur_axes(j,:)';
         angle = deg2rad(theta(j));
-        
+
         c = cos(angle); s = sin(angle);
         K = [0 -ax(3) ax(2); ax(3) 0 -ax(1); -ax(2) ax(1) 0];
         R = eye(3) + s*K + (1-c)*K*K;
-        
-        % Rotate EE position about this joint
+
         ee_pos = R*(ee_pos - origin) + origin;
-        
-        % Update downstream origins
+
         for k = j+1:6
             cur_origins(k,:) = (R*(cur_origins(k,:)' - origin) + origin)';
+            cur_axes(k,:) = (R*cur_axes(k,:)')';
         end
-        
-        % Update axes
-        for k = j+1:6
-            axes(k,:) = (R*axes(k,:)')';
-        end
-        
+
         R_total = R * R_total;
     end
 
     pos = ee_pos;
     rot = R_total;
+
+    % Extract RPY from rotation matrix columns
+    % X axis of EE frame
+    x_axis = rot(:,1);
+    y_axis = rot(:,2);
+    z_axis = rot(:,3);
+
+    % Yaw: rotation about Z (world)
+    yaw = atan2(x_axis(2), x_axis(1));
     
-    % Robust RPY
-    if abs(rot(3,1)) < 1 - 1e-10
-        pitch = asin(-rot(3,1));
-        roll  = atan2(rot(3,2)/cos(pitch), rot(3,3)/cos(pitch));
-        yaw   = atan2(rot(2,1)/cos(pitch), rot(1,1)/cos(pitch));
-    else
-        yaw = 0;
-        if rot(3,1) < 0
-            pitch = pi/2;
-            roll  = atan2(rot(1,2), rot(1,3));
-        else
-            pitch = -pi/2;
-            roll  = atan2(-rot(1,2), -rot(1,3));
-        end
-    end
+    % Pitch: rotation about Y (world)
+    pitch = atan2(-x_axis(3), sqrt(x_axis(1)^2 + x_axis(2)^2));
     
+    % Roll: rotation about X (world)
+    roll = atan2(y_axis(3), z_axis(3));
+
     rpy = rad2deg([roll; pitch; yaw]);
 end
